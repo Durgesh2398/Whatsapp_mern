b4c7185b06712944b3f1a297833319a0234fb093
Durgesh2398 <2308durgesh@gmail.com>
2020-09-14 23:23:40 +0530

    whatsapp clone

---
+++ whatsapp-mern/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCbFwbmrF6d_twzEHsfY7hdNiQel8cjs_Q",
+    authDomain: "whatsapp-mern-b64f4.firebaseapp.com",
+    databaseURL: "https://whatsapp-mern-b64f4.firebaseio.com",
+    projectId: "whatsapp-mern-b64f4",
+    storageBucket: "whatsapp-mern-b64f4.appspot.com",
+    messagingSenderId: "863386077911",
+    appId: "1:863386077911:web:9bed5aa5edaa11b7a0be92"
+  };
